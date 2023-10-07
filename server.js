28a20f87e31059fbf70665b266fc2bad37411ae9
19harshitha <harshithasm034@gmail.com>
2023-10-07 14:54:09 +0530

    added database config and initialzation

--- server.js
+++ server.js
@@ -9,6 +9,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
+
+app.get('/health', (req,res) => {
+    try {
+        res.status(200).json({ 
+            service: "job-listing-app",
+            status: "active",
+            time : new Date()
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: 'something went wrong'
+        })
+    }
+    
+} )
+
 app.listen (process.env.Port, () => {
+    mongoose
+    .connect(process.env.mongodb_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MongoDB Connected");
     console.log(`server is running on port ${process.env.Port}`)
 })
+.catch((err) => console.log("failed to connect" ,err) )
+});
