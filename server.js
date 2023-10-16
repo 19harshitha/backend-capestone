5e3ef5e253343aa5ffbb9cc7169028d2415fd711
19harshitha <harshithasm034@gmail.com>
2023-10-16 20:36:09 +0530

    changes

--- server.js
+++ server.js
@@ -44,16 +44,17 @@ app.use((req, res, next) => {
       .status(500)
       .json({ error: "Something went wrong! Please try after some time." });
   });
-
-app.listen (process.env.Port, () => {
+ 
+const PORT = process.env.PORT || 3000;
+app.listen (PORT, () => {
     mongoose
-    .connect(process.env.mongodb_URL, {
+    .connect(mongodb_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
     .then(() => {
         console.log("MongoDB Connected");
-    console.log(`server is running on port ${process.env.Port}`)
+    console.log(`server is running on port ${PORT}`)
 })
 .catch((err) => console.log("failed to connect" ,err) )
 });
